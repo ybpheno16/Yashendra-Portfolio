029ebacc4d4fd4978e354c2b1a48da04b9669070
ybpheno16 <yashubdl@gmail.com>
2024-07-05 21:49:54 +0530

    update the resume link

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = (props) => (
       <SectionText>
       A budding web developer with a passion for creative solutions. Interested in learning new tech stacks and programming languages.
       </SectionText>
-      <Button onClick={()=>{window.open("https://drive.google.com/file/d/1GWDj3J7EcXhStbvGUqsT7BfDy1ZCzt23/view?usp=sharing", "_blank");}}>Resume</Button>
+      <Button onClick={()=>{window.open("https://drive.google.com/file/d/1l3_M3_3_Zak2EQy65WYFwHvD7YD9HKVW/view?usp=sharing", "_blank");}}>Resume</Button>
     </LeftSection>
   </Section>
 );
